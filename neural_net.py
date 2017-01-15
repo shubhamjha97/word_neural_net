3a73950ed6d1dc659eaf193fbb3c4ae65d9f3640
Shubham Jha <jha1shubham@gmail.com>
2017-01-15 20:12:07 +0530

    training

--- neural_net.py
+++ neural_net.py
@@ -1,6 +1,8 @@
 import theano
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Merge
+from keras.optimizers import SGD
+from keras.metrics import top_k_categorical_accuracy
 
 class Net:
 	"""Sequential model
@@ -9,24 +11,28 @@ class Net:
 		'learn_features'
 		'unique_words'
 		"""
-	self.model_a=Sequential()
-	self.model_b=Sequential()
-	self.final_model=Sequential()
 
-	def __init__(input_dimension, act, hidden): #!!!! FIX THIS
+
+	def __init__(self, input_dimension, structure_dict): #!!!! FIX THIS
 		"""hidden= no of hidden units
 			input dimension-> tuple of input dim Eg:(784,1)
 			act-> string Eg: 'relu' """
-		self.model_a.add(Dense(hidden['distributed_encoding'], input_dim=input_dimension, activation='sigmoid'))
-		self.model_b.add(Dense(hidden['distributed_encoding'], input_dim=input_dimension, activation='sigmoid'))
+		self.model_a=Sequential()
+		self.model_b=Sequential()
+		self.final_model=Sequential()
+		self.model_a.add(Dense(structure_dict['distributed_encoding'], input_dim=input_dimension, activation='sigmoid'))
+		self.model_b.add(Dense(structure_dict['distributed_encoding'], input_dim=input_dimension, activation='sigmoid'))
 		self.merged=Merge([self.model_a, self.model_b], mode='concat')
-		self.final_model.add(merged)
-		self.final_model.add(Dense(hidden['learn_features'], activation='sigmoid'))
-		self.final_model.add(Dense(hidden['unique_words'], activation='softmax'))
-		self.final_model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-					metrics=['accuracy'])
+		self.final_model.add(self.merged)
+		self.final_model.add(Dense(structure_dict['learn_features'], activation='sigmoid'))
+		self.final_model.add(Dense(structure_dict['unique_words'], activation='softmax'))
+		self.sgd=SGD(lr=0.01)
+		def top_k(y_true, y_pred):
+			return top_k_categorical_accuracy(y_true, y_pred, k=10)
+		self.final_model.compile(optimizer=self.sgd, loss='categorical_crossentropy',
+					metrics=['accuracy',top_k ])
 
-	def train(train_X, train_y):
-		self.final_model.fit(train_X, train_y, nb_epoch= 10,  batch_size=50, validation_split=0.33)
+	def train(self, train_X_1, train_X_2, train_y):
+		self.final_model.fit([train_X_1, train_X_2], train_y, nb_epoch= 10,  batch_size=100, validation_split=0.33)
 
-	#Create an evaluate function
+	
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import numpy as np
 from nltk.tokenize import sent_tokenize, word_tokenize
+import neural_net
+from tqdm import tqdm
+import gc
+import sys
 
 words=[]
 with open('aliceinwonderland.txt', 'r') as f:
@@ -14,18 +18,66 @@ unique_words=set(words)
 word_dict=dict((w,i) for i,w in enumerate(unique_words))
 no_total_words=len(words)
 no_unique_words=len(word_dict)
-#print words
 
 print "Total words=" + str(no_total_words)
 print "Unique words=" + str(no_unique_words)
 
+del sentences #!!!!!!!!!
+
 #one-hot encoding
 def one_hot(word, word_dict):
-	temp=np.zeros([len(word_dict), 1])
+	temp=np.zeros(len(word_dict), dtype=np.int8)
 	i=word_dict[word]
-	print i
 	temp[i]=1
 	return temp
 
-print word_dict['is']
-print one_hot('is', word_dict)
+#print word_dict['is']
+#print one_hot('alice', word_dict)
+
+#Generate train data-- list of numpy arrays
+net_structure={'distributed_encoding':10, 'learn_features':30, 'unique_words':len(unique_words)}
+net=neural_net.Net(len(unique_words), net_structure)
+
+train_X_1=[]
+train_X_2=[]
+train_y=[]
+
+
+'''for w in tqdm(words[0:no_total_words-3]):
+	train_X_1.append(one_hot(w, word_dict))
+for w in tqdm(words[1:no_total_words-2]):
+	train_X_2.append(one_hot(w, word_dict))
+for w in tqdm(words[2:no_total_words-1]):
+	train_y.append(one_hot(w, word_dict))
+
+del words
+gc.collect()
+train_X_1=np.array(train_X_1)
+train_X_2=np.array(train_X_2)
+train_y=np.array(train_y)'''
+#net.train(train_X_1, train_X_2, train_y)
+
+#print train_X_1
+#print len(train_X_1)
+#print sys.getsizeof(train_X_1.shape)
+#print len(train_X_2)
+#print len(train_y)
+
+word1=one_hot('alice', word_dict)
+word2=one_hot('will', word_dict)
+
+def decode(word, word_dict):
+	idx=np.where(word==1)[0]
+	print idx
+	#return
+
+
+'''for i in xrange(100):
+	word3=net.predict([word1, word2])
+	print decode(word3, word_dict)
+	word1=word2
+	word2=word3'''
+
+if __name__=='__main__':
+	enc=one_hot('alice', word_dict)
+	decode(enc, word_dict)
